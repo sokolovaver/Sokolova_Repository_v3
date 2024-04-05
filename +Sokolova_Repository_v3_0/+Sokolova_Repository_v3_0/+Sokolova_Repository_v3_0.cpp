ab53b3e8a7e936198ecb77e163c5c0c1a2c942c8
Sokolova Veronika Alekseevna <veronika.sock@yandex.ru>
2024-04-05 08:23:02 +0300

    Functions

---
+++ +Sokolova_Repository_v3_0/+Sokolova_Repository_v3_0/+Sokolova_Repository_v3_0.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+
+using namespace std;
+
+double inputx() {
+
+	return 0;
+}
+
+double inputy() {
+
+	return 0;
+}
+
+double inputz() {
+
+	return 0;
+}
+
+int Summ(double x, double y, double z) {
+
+	return 0;
+}
+
+int Average(double x, double y, double z) {
+
+	return 0;
+}
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	int function;
+	cout << "1 - Ввести вещественное число x\n2 - Ввести вещественное число y\n3 - Ввести вещественное число z\n4 - Найти целую часть суммы этих 3-х чисел\n5 - Найти дробную часть среднего геометрического модулей этих 3-х чисел\n";
+	cout << "\nВведите номер функции: ";
+	cin >> function;
+
+	switch (function)
+	{
+	case 1:
+	{
+		break;
+	}
+	case 2:
+	{
+		break;
+	}
+	case 3:
+	{
+		break;
+	}
+	case 4:
+	{
+		break;
+	}
+	case 5:
+	{
+		break;
+	}
+	default:
+		break;
+	}
+}
