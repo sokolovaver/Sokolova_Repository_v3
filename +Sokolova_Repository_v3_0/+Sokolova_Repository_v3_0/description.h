74b4f640a1effc1719f39d375c56d410172d9e61
Sokolova Veronika Alekseevna <veronika.sock@yandex.ru>
2024-05-01 18:33:23 +0300

    Написание комментариев

---
+++ +Sokolova_Repository_v3_0/+Sokolova_Repository_v3_0/description.h
@@ -0,0 +1,17 @@
+/**
+	@mainpage Проект +Sokolova_Repository_v3_0 для тестирования Doxygen
+	Используется в лабораторной работе номер 3
+	по курсу Проектирование программного обеспечения
+	@copyright ВоГУ
+	@author Соколова В.А.
+	@date 01-05-2024
+\par Использует функции:
+- @ref main
+- @ref inputx
+- @ref inputy
+- @ref inputz
+- @ref Summ
+- @ref Average
+\par Содержит файлы:
+- @ref +Sokolova_Repository_v3_0.cpp
+*/
